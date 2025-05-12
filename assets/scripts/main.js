f930d14e13700a0a95608a86d8814c4ff9553846
lavinma <lavin.ma18@gmail.com>
2025-05-12 11:03:56 -0700

    Completed Part 1 of the lab

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,8 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	const recipes = localStorage.getItem('recipes');
+	return recipes ? JSON.parse(recipes) : [];
 }
 
 /**
@@ -35,10 +37,21 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const main = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	recipes.forEach((recipe) => {
+		// Create a new custom element <recipe-card>
+		const card = document.createElement('recipe-card');
+
+		// Set the recipe data into the card component
+		card.data = recipe;
+
+		// Append the card to the <main> element
+		main.appendChild(card);
+	});
 }
 
 /**
